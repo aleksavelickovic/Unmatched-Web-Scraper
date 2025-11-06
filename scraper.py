3d2b2937f66bac77e945b9e44ebf6e1c41fd5395
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-11-06 03:09:51 +0100

    [UPDATE] refactor scraper to utilize undetected-chromedriver and improve request handling

--- scraper.py
+++ scraper.py
@@ -1,63 +1,81 @@
+import base64
+import json
+from http.client import responses
+
+from encodings.utf_16 import decode
 from time import sleep
 
-import pytest
-import time
-
-from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
+
 from selenium.webdriver.support.ui import Select
-import chromedriver_autoinstaller
+
 import undetected_chromedriver as uc
 
 SLEEP_TIME = 25  # Prilagoditi broj shodno brzini vase internet konekcije
 
 
-# @pytest.fixture()
-# def browser():
-#     if chromedriver_autoinstaller.get_chrome_version() is None:
-#         chromedriver_autoinstaller.install()
-#     # neophodno odraditi ovo zbog problema sa CORS
-#     options = webdriver.ChromeOptions()
-#     options.add_argument("--disable-web-security")
-#     options.add_argument("--disable-site-isolation-trials")
-#     # kreiraj instancu drajvera za test
-#     driver = uc.Chrome(options=options)
-#
-#     driver.implicitly_wait(10)
-#
-#     yield driver
-#
-#     driver.quit()
+def get_matching_requests(browser):
+    events = browser.get_log("performance")
+    results = []
+
+    for e in events:
+        message = json.loads(e["message"])["message"]
+        method = message.get("method", "")
+        params = message.get("params", {})
+        # response = params.get("response", {})
+        # status = response.get("statusText")
+        # print("STATUS ODGOVORA JE: " + status)
+
+        if method == "Network.responseReceived":
+            url = params["response"]["url"]
+            if "https://www.umleague.net/api/analytics/getHeroResultsByMap" in url:  # TODO vrati samo uspesne odgovore!
+                results.append(params["requestId"])
+
+    return results
 
 
-def test_scrape_page():
-    options = webdriver.ChromeOptions()
-    # neophodno odraditi ovo zbog problema sa CORS
-    options.add_argument("--disable-web-security")
-    options.add_argument("--disable-site-isolation-trials")
+def get_response_body(browser, request_id):
+    try:
+        body = browser.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id})
+        data = body["body"]
+        if body.get("base64Encoded", False):
+            data = base64.b64decode(data).decode("utf-8")
+        return data
+    except:
+        return None
+
+
+def test_scrape_page_v3():
+    options = uc.ChromeOptions()
+    options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
+
     browser = uc.Chrome(options=options)
+    browser.execute_cdp_cmd("Network.enable", {})
     browser.implicitly_wait(10)
 
     browser.get("https://www.umleague.net/fighterstats")
+    sleep(15)
 
-    select_hero_element = browser.find_element(By.ID, 'selectHero')
-    # select_hero_element.click()
-    browser.execute_script("arguments[0].scrollIntoView();",
-                           select_hero_element)  # Nije obavezno ali cisto da se vidi sta se desava
-
-    limiter = 0
+    select_hero_element = browser.find_element(By.ID, "selectHero")
+    browser.execute_script("arguments[0].scrollIntoView();", select_hero_element)
     select_hero = Select(select_hero_element)
-    for option in select_hero.options:
-        while limiter < 12:
-            option.click()
-            sleep(SLEEP_TIME)
-            limiter += 1
 
-    # for request in driver.requests
+    for i, option in enumerate(select_hero.options):
+        if i >= 2:
+            break
+        option.click()
+        sleep(SLEEP_TIME)
+
+    request_ids = get_matching_requests(browser)
+
+    print(f"Captured {len(request_ids)} API calls")
+
+    for idx, req_id in enumerate(request_ids, start=1):
+        body = get_response_body(browser, req_id)
+        if body:
+            filename = f"hero_results_{idx}.json"
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(body)
+            print(f"[+] Saved → {filename}")
 
     browser.quit()
-
-# sleep(5)
