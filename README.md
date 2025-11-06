840926a632ee86b778f52f39b630efb66d082f4e
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-11-06 01:21:50 +0100

    [ADD] update README with installation instructions and add requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Unmatched Web Scraper
 
 Web scraper for hero statistics from the Unmatched board game series.
+
+PIP Command:
+
+```pip install -r requirements.txt```
--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from time import sleep
 import pytest
 import time
 
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -14,29 +14,44 @@ import chromedriver_autoinstaller
 SLEEP_TIME = 25  # Prilagoditi broj shodno brzini vase internet konekcije
 
 
-@pytest.fixture()
-def browser():
-    if chromedriver_autoinstaller.get_chrome_version() is None:
-        chromedriver_autoinstaller.install()
-    # neophodno odraditi ovo zbog problema sa CORS
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-web-security")
-    options.add_argument("--disable-site-isolation-trials")
-    # kreiraj instancu drajvera za test
-    driver = webdriver.Chrome(options=options)
+# @pytest.fixture()
+# def browser():
+#     if chromedriver_autoinstaller.get_chrome_version() is None:
+#         chromedriver_autoinstaller.install()
+#     # neophodno odraditi ovo zbog problema sa CORS
+#     options = webdriver.ChromeOptions()
+#     options.add_argument("--disable-web-security")
+#     options.add_argument("--disable-site-isolation-trials")
+#     # kreiraj instancu drajvera za test
+#     driver = webdriver.Chrome(options=options)
+#
+#     driver.implicitly_wait(10)
+#
+#     yield driver
+#
+#     driver.quit()
 
-    driver.implicitly_wait(10)
 
-    yield driver
+def test_scrape_page():
+    browser = webdriver.Chrome()
 
-    driver.quit()
-
-
-def test_scrape_page(browser: 'WebDriver'):
     browser.get("https://www.umleague.net/fighterstats")
 
     sleep(10)
 
-    select_hero = browser.find_element(By.ID, 'selectHero')
-    select_hero.click()
-    sleep(5)
+    select_hero_element = browser.find_element(By.ID, 'selectHero')
+    # select_hero_element.click()
+    browser.execute_script("arguments[0].scrollIntoView();",
+                           select_hero_element)  # Nije obavezno ali cisto da se vidi sta se desava
+
+    limiter = 0
+    select_hero = Select(select_hero_element)
+    for option in select_hero.options:
+        while limiter < 2:
+            option.click()
+            sleep(SLEEP_TIME)
+            limiter += 1
+
+    # for request in browser.requests
+
+# sleep(5)
